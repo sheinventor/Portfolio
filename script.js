a5607e29a507fad071391ec21531d63795468d80
SheInventor <paola.vaz.bar@gmail.com>
2025-03-18 21:31:09 +0100

    Holale

---
+++ script.js
@@ -0,0 +1,7 @@
+function searchFunction() {
+    let query = document.getElementById("search").value;
+    if (query) {
+        alert("Buscando: " + query);
+        // Aquí puedes redirigir a una página de resultados o filtrar contenido dinámicamente
+    }
+}
